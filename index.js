edb2b81864148cdadcd850cd617bccb308699eda
S10275014 <S10275014@connect.np.edu.sg>
2026-03-25 09:17:31 +0800

    beta

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-import express from "express";
+import express, { request, response } from "express";
+import data from "./data/mock.json" with { type: "json" };
 
 //const express = require("express");
 const app = express();
 const PORT = 3000;
+//GET
+app.get("/", (request, response) => {
+  response.send("This is a GET request at /");
+});
+//POST
+app.post("/create", (request, response) => {
+  response.send("This is a POST request at /create");
+});
+//PUT
+app.put("/edit", (request, response) => {
+  response.send("This is a PUT request at /edit");
+});
+//DELETE
+app.delete("/delete", (request, response) => {
+  response.send("This is a DELETE request at /delete");
+});
 
 app.listen(PORT, () => {
   console.log(`The Server is running on port ${PORT}`);
+  console.log(data);
 });
