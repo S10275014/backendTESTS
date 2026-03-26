fae7536db1cd72b17dfb04c7611e65224bf206af
S10275014 <S10275014@connect.np.edu.sg>
2026-03-26 15:00:12 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -9,6 +9,42 @@ const PORT = 3000;
 //   response.send("This is a GET request at /");
 // });
 
+//GET with next()
+app.get(
+  "/next",
+  (request, response, next) => {
+    console.log("The response will be sent by the next function.");
+    next();
+  },
+  (request, response) => {
+    response.send("I just set up a route with a second callback");
+  },
+);
+
+app.get("/animals", (request, response) => {
+  response.json(data);
+});
+
+//GET with Routing Parameters
+app.get("/class/:id", (request, response) => {
+  const studentID = Number(request.params.id);
+
+  const student = data.filter((student) => student.id === studentID);
+
+  response.send(student);
+});
+
+//app.get('users/:userId/books/:bookId', function (req, res) {
+//response.send(req.params)
+//})
+
+app.get("/chatgpt", (request, response) => {
+  response.json({
+    greeting: "Hello World",
+    humans: data,
+  });
+});
+
 //using the public folder at the root of the project
 app.use(express.static("public"));
 
