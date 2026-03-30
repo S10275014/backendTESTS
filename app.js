557fd5206ae8c442bd3d65658f111b210a93ad93
S10275014 <S10275014@connect.np.edu.sg>
2026-03-30 17:04:02 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// function sayHello(name) {
+//   console.log("Hello " + name);
+// }
+
+// //sayHello("Mosh");
+// console.log(window);
+
+console.log(); //global
+
+setTimeout();
+clearTimeout();
+
+setInterval();
+clearInterval();
+
+//window.console.log;
+//window.setTimeout
+
+var message = "";
+
+//window.message;
