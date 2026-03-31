fcc675f1c44ad100475a9b92e4bd1f13ee5b04ad
S10275014 <S10275014@connect.np.edu.sg>
2026-03-31 11:54:29 +0800

    Add files via upload

--- logger.js
+++ logger.js
@@ -1,3 +1,32 @@
+// (function (exports, require, module, __filename, __dirname)) {
+//   var url = "https://chatgpt.com/";
+
+//     function log(message) {
+//       //Send an http request
+//       console.log(message);
+//     }
+
+// module.exports = log;
+// }
+// (function (exports, require, module, __filename, __dirname) {
+//   var url = "https://chatgpt.com/";
+
+//   function log(message) {
+//     // Send an HTTP request
+//     console.log(message);
+//   }
+
+//   // module.exports = log;
+
+//   // module.exports.log = log;
+//   // exports.log = log;
+
+//   // exports = log; //module.exports
+// });
+// (exports, require, module, __filename, __dirname);
+console.log(__filename);
+console.log(__dirname);
+//var x =;
 var url = "https://chatgpt.com/";
 
 function log(message) {
