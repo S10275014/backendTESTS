40ae20cfeffedf0891d3c4a2be497c601cf4c02d
S10275014 <S10275014@connect.np.edu.sg>
2026-04-02 11:10:34 +0800

    Add files via upload

--- apps.js
+++ apps.js
@@ -1,1 +1,11 @@
-console.log("hello world from nodejs");
+//console.log("hello world from nodejs");
+
+const tutorial = require("./tutorial");
+console.log(tutorial);
+//sum(1, 1);
+
+// console.log(tutorial(1, 1));
+
+console.log(tutorial.sum(1, 1));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathObject());
