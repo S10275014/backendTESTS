ab348642f249cd60b27fc7494993648d5687688f
S10275014 <S10275014@connect.np.edu.sg>
2026-04-02 13:12:21 +0800

    Add files via upload

---
+++ global.js
@@ -0,0 +1,24 @@
+//Global objects
+
+//test in browser
+// window.setTimeout(() => {
+//   alert("hello");
+// }, 3000);
+
+console.log(global);
+
+// global.setTimeout(() => {
+//   console.log("in the timeout");
+// }, 3000);
+
+// setTimeout(() => {
+//   console.log("in the timeout");
+//   clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//   console.log("In the interval");
+// }, 1000);
+
+console.log(__dirname);
+console.log(__filename);
